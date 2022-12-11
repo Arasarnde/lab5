bf3a4c262d62fc794630b38c04226b6fa47df8ab
Sonya <115939170+Arasarnde@users.noreply.github.com>
2022-12-11 22:25:46 +0300

    Add files via upload

---
+++ 1.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Дек 11 2022 г., 20:25
+-- Версия сервера: 10.4.24-MariaDB
+-- Версия PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `admin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `1`
+--
+
+CREATE TABLE `1` (
+  `id` int(255) NOT NULL,
+  `name` text NOT NULL,
+  `cost` int(255) NOT NULL,
+  `img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `1`
+--
+
+INSERT INTO `1` (`id`, `name`, `cost`, `img`) VALUES
+(1, 'one', 111111, 'pictures/succulent5.png'),
+(2, 'three', 333333, 'pictures/succulent3.png'),
+(4, '5555', 5555, 'pictures/succulent1.png'),
+(5, '5665', 5555, 'pictures/succulent5.png');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
